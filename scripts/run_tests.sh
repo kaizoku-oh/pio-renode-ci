12da4ce6ca8e260882915ab68918648224063ea7
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-02-08 00:14:36 -0800

    add debug msg to ci

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,5 +3,5 @@
 # Runs Robot Framework tests locally
 
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
-
+ls ${RENODE_CHECKOUT}
 ${RENODE_CHECKOUT}/test.sh -t "${PWD}/../test/tests.yaml" --variable PWD_PATH:"${PWD}/.." -r "${PWD}/../test_results"

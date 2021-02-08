919e9df1ec7b33a31ecc78ed6e24090888841229
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-02-08 00:20:40 -0800

    adding msgs to ci again

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,5 +3,9 @@
 # Runs Robot Framework tests locally
 
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
+
+pwd
 ls -la
+ls -la ../
+
 ${RENODE_CHECKOUT}/test.sh -t "${PWD}/../test/tests.yaml" --variable PWD_PATH:"${PWD}/.." -r "${PWD}/../test_results"
